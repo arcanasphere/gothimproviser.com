dc8f9d30fab63891b753a4cbf981a08e5d2775ba
arcanasphere <amjohnston@arcanasphere.com>
2017-06-21 22:01:09 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,159 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Goth Improviser</title>
+
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Bootstrap Below -->
+  <!-- compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+
+  <!-- Custom CSS, load after Bootstrap code. -->
+  <link rel="stylesheet" href="css/gothimp.css" />
+
+  <meta property="og:site_name" content="Goth Improviser" />
+  <meta property="og:title" content="Andrew Johnston" />
+  <meta property="og:image" content="http://gothimproviser.com/images/roses-and-candles.jpg" />
+  <meta property="og:type" content="article" />
+  <meta property="og:description" content="Austin's Gloomiest Improvisational Thespian" />
+  <meta property="og:url" content="http://gothimproviser.com/" />
+
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="Goth Improviser">
+  <meta name="twitter:image" content="http://gothimproviser.com/images/roses-and-candles.jpg">
+  <meta name="twitter:description" content="Andrew Johnston. Austin's Gloomiest Improvisational Thespian">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-fixed-top" id="TopNavigation">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#GothImprovNavBar" aria-expanded="false" aria-controls="GothImprovNavBar" id="PushTheButton">
+          <span class="sr-only">Toggle navigation</span>
+          <span id="MobileMenu" class="glyphicon glyphicon-menu-hamburger"></span>
+        </button>
+        <a class="navbar-brand" href="#TheLastDayIWasHappy">Goth Improviser</a>
+        <div id="GothImprovNavBar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="#TheFlatFields">Improv</a></li>
+              <li><a href="#UncertainJourney">Music</a></li>
+              <li><a href="#SilverKnives">Existential Crisis (FAQ)</a></li>
+            </ul>
+            <ul class="nav navbar-right hidden-xs" id="TopNavGothName">
+              <li>Andrew M. Johnston</li>
+            </ul>
+        </div> <!-- end navbar-collapse -->
+  </nav> <!-- end navbar nav -->
+
+
+  <div id="Hide-Behind-Nav"></div>
+
+
+  <div class="jumbotron fixedbg no-vertical" id="BlackNumber1">
+    <div class="container-fluid" id="TheLastDayIWasHappy">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>Andrew M. Johnston</h1>
+          <h2>Goth Improviser</h2>
+          <p>Austin's Gloomiest Improvisational Thespian.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="jumbotron no-vertical" id="TheFlatFields">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <h3 class="rocker">Improv</h3>
+        </div>
+        <div class="col-sm-2 col-md-3 sm-left">
+          <img src="/images/goth-cowboy.png" alt="Backstage at Buzz Band" class="gentle-size" />
+        </div>
+        <div class="col-sm-10 col-md-9">
+          <p>Andrew has studied, performed, and served as a teacher's assistant at the <b>Institution Theatre</b> in Austin, TX.</p>
+          <p>Since 2012.</p>
+          <p>Most notably played drums &amp; guitar in the alternative rock themed <a class="rocker" href="http://www.steve-rogers-photography.com/ImprovPerformances/The-Institution-Theater/Buzz-Band-May-8-2015-Opening/i-nmL9CkJ/A" target="_blank">Buzz Band</a>.</p>
+          <p>One time winner of the <b class="rocker">Austin Monologue Jam</b>.</p>
+          <p>Founder of the improv troupe <a class="rocker" href="http://candyvampires.com" target="_blank">Candy Vampires</a>.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="jumbotron fixedbg no-vertical" id="BlackNumber2">
+  </div>
+
+
+  <div class="jumbotron no-vertical" id="UncertainJourney">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <h3 class="amatica">Music</h3>
+        </div>
+        <div class="col-sm-3 col-md-2 col-sm-push-9 col-md-push-10 sm-right">
+          <a href="https://arcanasphere.bandcamp.com/album/the-whitest-rose" target="blank"><img src="/images/arcanasphere-whitest-rose.jpg" alt="The Whitest Rose by Arcanasphere" class="gentle-size" /></a>
+        </div>
+        <div class="col-sm-9 col-md-10 col-sm-pull-3 col-md-pull-2">
+          <p>In his spare time, Andrew records music under the name <b class="rocker">Arcanasphere</b>.</p>
+          <p>His music has appeared in two charity bundles at the site Groupees.com, and has been played on WSGR in Port Huron, Michigan.</p>
+          <p>He writes moody music with heavy reverb on the guitars. There's a rose on the album cover.  It can't get more goth than this.</p>
+        </div>
+      </div> <!-- end inner row -->
+    </div>
+  </div>
+
+  <div class="jumbotron fixedbg no-vertical" id="BlackNumber3">
+  </div>
+
+  <div class="jumbotron no-vertical" id="SilverKnives">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12">
+          <h3 class="rocker">Existential Crisis <b class="buda">(FAQ)</b></h3>
+          <h4 class="amatica">Is this serious?</h4>
+          <p>Yes, more or less.  People reached out to say they preceived me as "the goth improviser."  Others volunteered as much when I asked for opinions.</p>
+          <p>If this is my personal brand in the improv community then I will own it <b>in full</b>.</p>
+
+          <h4 class="amatica">Andrew's Offerings To You</h4>
+          <p>I am comfortable if not happy providing both musical and emotional accompaniment to your improv.</p>
+          <h4 class="amatica">Contact</h4>
+          <div class="row">
+            <div class="col-sm-6 text-center">
+              <h4 class="rocker">FaceBook</h4>
+              <p><b><a href="https://facebook.com/candyvampires">Candy Vampires</a></b></p>
+              <p><b><a href="https://www.facebook.com/arcanasphere-music">Arcanasphere</a></b></p>
+            </div>
+            <div class="col-sm-6 text-center">
+              <h4 class="rocker">Voice Mail</h4>
+              <p><b>512-953-7935</b></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="jumbotron no-vertical" id="AllThingsEnd">
+    &copy; 2017 Andrew M Johnston
+  </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+</body>
+
+
+</html>
